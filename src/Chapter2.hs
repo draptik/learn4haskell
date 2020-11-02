3215c0777842d4b65fafd4b9e02ba2df14b6bda0
Patrick Drechsler <patrick@pdrechsler.de>
2020-11-02 20:31:23 +0100

    ch2: task4: primitive solution

--- src/Chapter2.hs
+++ src/Chapter2.hs
@@ -361,9 +361,13 @@ Implement a function that returns only the first half of a given list.
 >>> firstHalf "bca"
 "b"
 -}
--- PUT THE FUNCTION TYPE IN HERE
-firstHalf l = error "firstHalf: Not implemented!"
-
+firstHalf :: [a] -> [a]
+firstHalf l =
+  let
+    lengthOfCollection = length l
+    lengthOfFirstHalf = div lengthOfCollection 2
+  in
+    take (lengthOfFirstHalf) l
 
 {- |
 =🛡= Pattern matching
